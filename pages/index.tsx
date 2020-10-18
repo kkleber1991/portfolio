6e85ff9cbee3a3965014b68e899202ffb0ca59e9
kkleber1991 <kkleber.1991@hotmail.com>
2020-10-18 11:37:06 -0700

    finalizando

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Flex, Box } from '@chakra-ui/core';
+import { Flex, Box, Image } from '@chakra-ui/core';
 import Home from '../components/Home'
 
 function pages() {
   return (
     <Flex maxW="100%" justify="center">
+      <Image
+      src="../assets/background.jpg"
+      fallbackSrc="sldkfj.com/200x200" />
       <Box w="90%" pt={8} ><Home /></Box>
     </Flex>
   );
