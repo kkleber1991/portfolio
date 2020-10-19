bb7e88943f04175e3e8b1d2bce9752fbed9e78b1
kkleber1991 <kkleber.1991@hotmail.com>
2020-10-18 17:07:09 -0700

    finalizando

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,10 +1,36 @@
-import { Box, Text } from '@chakra-ui/core'
+import { Box, Text, SimpleGrid } from '@chakra-ui/core'
 import React from 'react'
 
 export default function Portfolio() {
     return (
         <Box bg="white" >
-            <Text align="center" fontSize="4xl" fontWeight="bold" color="#FEAE0B" >PÁGINA EM CONSTRUÇÃO</Text>
+            <SimpleGrid columns={2} spacingX="40px" spacingY="40px" p={6} >
+                <Box>
+                    <Box backgroundImage="url('/images/efop.png')" h={[80,140,190,240]} bgSize="100%">
+                    </Box>
+                    <Text fontWeight="bold" align="center">EFOP</Text>
+                </Box>
+                <Box>
+                    <Box backgroundImage="url('/images/insidecontabilidade.png')" h={[80,140,190,240]} bgSize="100%">
+                    </Box>
+                    <Text fontWeight="bold" align="center">INSIDE CONTABILIDADE</Text>
+                </Box>
+                <Box>
+                    <Box backgroundImage="url('/images/manualdamamae.png')" h={[80,140,190,240]} bgSize="100%">
+                    </Box>
+                    <Text fontWeight="bold" align="center">MANUAL DA MAMAE</Text>
+                </Box>
+                <Box>
+                    <Box backgroundImage="url('/images/movesistemas.png')" h={[80,140,190,240]} bgSize="100%">
+                    </Box>
+                    <Text fontWeight="bold" align="center">MOVE SISTEMAS</Text>
+                </Box>
+                <Box>
+                    <Box backgroundImage="url('/images/rapidopark.png')" h={[80,140,190,240]} bgSize="100%">
+                    </Box>
+                    <Text fontWeight="bold" align="center">RAPIDO PARK</Text>
+                </Box>
+            </SimpleGrid>
         </Box>
     )
 }
